8766af3cc4d3b1a41896883508f0a98263972cdc
MatthewDooler <MatthewDooler@users.noreply.github.com>
2015-05-16 23:37:54 +0100

    Formatting

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-from pyparsing import Literal, Word, Optional, ZeroOrMore, Empty, Group, Combine, alphas, countedArray
+from pyparsing import Literal, Word, Optional, ZeroOrMore, Empty, Group, Combine, Forward, alphas, countedArray
 
 # XQuery Grammar: http://www.w3.org/TR/xquery/
 
@@ -12,38 +12,40 @@ StringLiteral = (Literal('"') + String + Literal('"')) | Literal("'") + String +
 VersionDecl = Literal("xquery") + Literal("version") + StringLiteral + Optional(Literal("encoding") + StringLiteral) + Separator
 
 # QName
-NameStartChar = Word(alphas + "_")
-NameChar = Word(alphas + "_-.0123456789")
-Name = NameStartChar + ZeroOrMore(NameChar)
-NCName = Combine(Name)
-LocalPart = NCName.setResultsName("localpart")
-Prefix = NCName.setResultsName("prefix")
-UnprefixedName = LocalPart
-PrefixedName = Prefix + Literal(":") + LocalPart
-QName = Combine(PrefixedName | UnprefixedName)
+NameStartChar   = Word(alphas + "_")
+NameChar        = Word(alphas + "_-.0123456789")
+Name            = NameStartChar + ZeroOrMore(NameChar)
+NCName          = Combine(Name)
+LocalPart       = NCName.setResultsName("localpart")
+Prefix          = NCName.setResultsName("prefix")
+UnprefixedName  = LocalPart
+PrefixedName    = Prefix + Literal(":") + LocalPart
+QName           = Combine(PrefixedName | UnprefixedName)
+VarName         = QName
+VarRef          = Literal("$") + VarName
 
 # Types and occurrence indicators
-AnyKindTest = Literal("node") + Literal("(") + Literal(")")
-TextTest = Literal("text") + Literal("(") + Literal(")")
-CommentTest = Literal("comment") + Literal("(" ")")
-PITest = Literal("processing-instruction") + Literal("(") + Optional(NCName | StringLiteral) + Literal(")")
-TypeName = QName
-AttributeName = QName
-AttribNameOrWildcard = AttributeName | Literal("*")
-ElementName = QName
+AnyKindTest           = Literal("node") + Literal("(") + Literal(")")
+TextTest              = Literal("text") + Literal("(") + Literal(")")
+CommentTest           = Literal("comment") + Literal("(" ")")
+PITest                = Literal("processing-instruction") + Literal("(") + Optional(NCName | StringLiteral) + Literal(")")
+TypeName              = QName
+AttributeName         = QName
+AttribNameOrWildcard  = AttributeName | Literal("*")
+ElementName           = QName
 ElementNameOrWildcard = ElementName | Literal("*")
-AttributeDeclaration = AttributeName
-SchemaAttributeTest = Literal("schema-attribute") + Literal("(") + AttributeDeclaration + Literal(")")
-AttributeTest = Literal("attribute") + Literal("(") + Optional(AttribNameOrWildcard + Optional(Literal(",").suppress() + TypeName)) + Literal(")")
-ElementDeclaration = ElementName
-SchemaElementTest = Literal("schema-element") + Literal("(") + ElementDeclaration + Literal(")")
-ElementTest = Literal("element") + Literal("(") + Optional(ElementNameOrWildcard + Optional(Literal(",").suppress() + TypeName + Optional(Literal("?")))) + Literal(")")
-DocumentTest = Literal("document-node") + Literal("(") + Optional(ElementTest | SchemaElementTest) + Literal(")")
-KindTest = DocumentTest | ElementTest | AttributeTest | SchemaElementTest | SchemaAttributeTest | PITest | CommentTest | TextTest | AnyKindTest
-AtomicType = QName
-OccurrenceIndicator = (Literal("?") | Literal("*") | Literal("+")).setResultsName("occurrence")
-ItemType = (KindTest | (Literal("item") + Literal("(") + Literal(")")) | AtomicType).setResultsName("type")
-SequenceType = (Literal("empty-sequence") + Literal("(") + Literal(")")) | (ItemType + Optional(OccurrenceIndicator))
+AttributeDeclaration  = AttributeName
+SchemaAttributeTest   = Literal("schema-attribute") + Literal("(") + AttributeDeclaration + Literal(")")
+AttributeTest         = Literal("attribute") + Literal("(") + Optional(AttribNameOrWildcard + Optional(Literal(",").suppress() + TypeName)) + Literal(")")
+ElementDeclaration    = ElementName
+SchemaElementTest     = Literal("schema-element") + Literal("(") + ElementDeclaration + Literal(")")
+ElementTest           = Literal("element") + Literal("(") + Optional(ElementNameOrWildcard + Optional(Literal(",").suppress() + TypeName + Optional(Literal("?")))) + Literal(")")
+DocumentTest          = Literal("document-node") + Literal("(") + Optional(ElementTest | SchemaElementTest) + Literal(")")
+KindTest              = DocumentTest | ElementTest | AttributeTest | SchemaElementTest | SchemaAttributeTest | PITest | CommentTest | TextTest | AnyKindTest
+AtomicType            = QName
+OccurrenceIndicator   = (Literal("?") | Literal("*") | Literal("+")).setResultsName("occurrence")
+ItemType              = (KindTest | (Literal("item") + Literal("(") + Literal(")")) | AtomicType).setResultsName("type")
+SequenceType          = (Literal("empty-sequence") + Literal("(") + Literal(")")) | (ItemType + Optional(OccurrenceIndicator))
 
 # Parameters
 TypeDeclaration = Literal("as") + SequenceType
@@ -51,10 +53,57 @@ Param           = Group(Literal("$").suppress() + QName.setResultsName("name") +
 ParamList       = Param + ZeroOrMore(Literal(",").suppress() + Param)
 
 # Expressions
-QueryBody     = Expr
-Expr          = ExprSingle ("," ExprSingle)*
-ExprSingle    = FLWORExpr | QuantifiedExpr | TypeswitchExpr | IfExpr | OrExpr
-EnclosedExpr  = Literal("{").suppress() + Expr + Literal("}").suppress()
+ExprSingle      = Forward()
+Expr            = Forward()
+ValidationMode  = Literal("lax") | Literal("strict")
+NodeComp        = Literal("is") | Literal("<<") | Literal(">>")
+ValueComp       = Literal("eq") | Literal("ne") | Literal("lt") | Literal("le") | Literal("gt") | Literal("ge")
+GeneralComp     = Literal("=") | Literal("!=") | Literal("<") | Literal("<=") | Literal(">") | Literal(">=")
+ValidateExpr    = Literal("validate") + Optional(ValidationMode) + Literal("{") + Expr + Literal("}")
+
+# TODO: this is yao next step.. impl these and follow the chain
+StepExpr            = FilterExpr | AxisStep
+RelativePathExpr    = StepExpr + ZeroOrMore((Literal("/") | Literal("//")) + StepExpr)
+PathExpr            = (Literal("/") + Optional(RelativePathExpr)) | (Literal("//") + RelativePathExpr) | RelativePathExpr
+#PragmaContents     = (Char* - (Char* '#)' Char*)) # TODO: wtf
+#S                  = (#x20 | #x9 | #xD | #xA)+ # TODO: wtf
+PragmaContents      = Word(alphas) # TODO: just a terrible guess
+S                   = Word(alphas) # TODO: just a terrible guess
+Pragma              = Literal("(#") + Optional(S) + QName + Optional(S + PragmaContents) + Literal("#)")
+ExtensionExpr       = Pragma + Literal("{") + Optional(Expr) + Literal("}")
+ValueExpr           = ValidateExpr | PathExpr | ExtensionExpr
+UnaryExpr           = ZeroOrMore(Literal("-") | Literal("+")) + ValueExpr
+CastExpr            = UnaryExpr + Optional(Literal("cast") + Literal("as") + SingleType)
+CastableExpr        = CastExpr + Optional(Literal("castable") + Literal("as") + SingleType)
+TreatExpr           = CastableExpr + Optional(Literal("treat") + Literal("as") + SequenceType)
+InstanceofExpr      = TreatExpr + Optional(Literal("instance") + Literal("of") + SequenceType)
+IntersectExceptExpr = InstanceofExpr + ZeroOrMore((Literal("intersect") | Literal("except")) + InstanceofExpr)
+UnionExpr           = IntersectExceptExpr + ZeroOrMore(("union") | Literal("|") + IntersectExceptExpr)
+MultiplicativeExpr  = UnionExpr + ZeroOrMore((Literal("*") | Literal("div") | Literal("idiv") | Literal("mod")) + UnionExpr)
+AdditiveExpr        = MultiplicativeExpr + ZeroOrMore((("+") | Literal("-")) + MultiplicativeExpr)
+RangeExpr           = AdditiveExpr + Optional(Literal("to") + AdditiveExpr)
+ComparisonExpr      = RangeExpr + Optional((ValueComp | GeneralComp | NodeComp) + RangeExpr)
+AndExpr             = ComparisonExpr + ZeroOrMore(Literal("and") + ComparisonExpr)
+OrExpr              = AndExpr + ZeroOrMore(Literal("or") + AndExpr)
+
+QuantifiedExpr  = Literal("some") | Literal("every") + Literal("$") + VarName + Optional(TypeDeclaration) + Literal("in") + ExprSingle + ZeroOrMore(Literal(",") + Literal("$") + VarName + Optional(TypeDeclaration) + Literal("in") + ExprSingle) + Literal("satisfies") + ExprSingle
+OrderModifier   = Optional(Literal("ascending") | Literal("descending")) + Optional(Literal("empty") + (Literal("greatest") | Literal("least"))) + Optional(Literal("collation") + URILiteral)
+OrderSpec       = ExprSingle + OrderModifier
+OrderSpecList   = OrderSpec + ZeroOrMore(Literal(",") + OrderSpec)
+OrderByClause   = (("order") + Literal("by") | Literal("stable") + Literal("order") + Literal("by")) + OrderSpecList
+WhereClause     = Literal("where") + ExprSingle
+LetClause       = Literal("let") + Literal("$") + VarName + Optional(TypeDeclaration) + Literal(":=") + ExprSingle + ZeroOrMore(Literal(",") + Literal("$") + VarName + Optional(TypeDeclaration) + Literal(":=") + ExprSingle)
+PositionalVar   = Literal("at") + Literal("$") + VarName
+ForClause       = Literal("for") + Literal("$") + VarName + Optional(TypeDeclaration) + Optional(PositionalVar) + Literal("in") + ExprSingle + ZeroOrMore(Literal(",") + Literal("$") + VarName + Optional(TypeDeclaration) + Optional(PositionalVar) + Literal("in") + ExprSingle)
+FLWORExpr       = OneOrMore(ForClause | LetClause) + Optional(WhereClause) + Optional(OrderByClause) + Literal("return") + ExprSingle
+
+IfExpr          = Literal("if") + Literal("(") + Expr + Literal(")") + Literal("then") + ExprSingle + Literal("else") + ExprSingle
+CaseClause      = Literal("case") + Optional(Literal("$") + VarName + Literal("as")) + SequenceType + Literal("return") + ExprSingle
+TypeswitchExpr  = Literal("typeswitch") + Literal("(") + Expr + Literal(")") + OneOrMore(CaseClause) + Literal("default") + Optional(Literal("$") + VarName) + Literal("return") + ExprSingle
+ExprSingle    <<= (FLWORExpr | QuantifiedExpr | TypeswitchExpr | IfExpr | OrExpr)
+Expr          <<= (ExprSingle + ZeroOrMore(Literal(",") + ExprSingle))
+QueryBody       = Expr
+EnclosedExpr    = Literal("{").suppress() + Expr + Literal("}").suppress()
 
 # Prolog: Imports
 URILiteral    = StringLiteral # TODO: StringLiteral will need to include slashes etc
@@ -83,7 +132,7 @@ FunctionDecl      = Group(Literal("declare") + Literal("function") + QName.setRe
 Setter = BoundarySpaceDecl | DefaultCollationDecl | BaseURIDecl | ConstructionDecl | OrderingModeDecl | EmptyOrderDecl | CopyNamespacesDecl
 Prolog = ZeroOrMore((DefaultNamespaceDecl | Setter | NamespaceDecl | Import) + Separator) + ZeroOrMore((VarDecl | FunctionDecl | OptionDecl) + Separator)
 
-QueryBody = Word(alphas) # TODO
+#QueryBody = Word(alphas) # TODO
 ModuleDecl = Word(alphas) # TODO
 
 LibraryModule   = ModuleDecl + Prolog
